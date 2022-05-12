0d6c1e41db4eef5a6fd365ef34a949ef86bf6afa
Josua Jonathan Manurung <87791029+josuajonathan@users.noreply.github.com>
2022-05-12 07:34:39 +0700

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,20 @@
+#include "Player.h"
+
+Player::Player() {
+	username = "nama";
+}
+
+void Player::setusername(string username)
+{
+	this->username = username;
+}
+
+string Player::getusername()
+{
+	return username;
+}
+
+void Player::getscore()
+{
+
+}
