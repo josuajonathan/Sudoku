59edcc5e0bb7ec8bb9140ac2bbc131e10df57268
Josua Jonathan Manurung <87791029+josuajonathan@users.noreply.github.com>
2022-05-12 08:00:53 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Player.h"
+using namespace std;
+
+int main()
+{
+	string username;
+	"Welcome to Sudoku";
+	cout << "Input nama Pemain : " << endl;
+	cin >> username;
+}
